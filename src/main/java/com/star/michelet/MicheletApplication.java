d6a43a0670eb9c65b1df31e432e9a17c427ba25d
jihoon <git4ub0@gmail.com>
2026-04-24 08:48:51 +0900

    Init: 프로젝트 초기 설정

---
+++ src/main/java/com/star/michelet/MicheletApplication.java
@@ -0,0 +1,13 @@
+package com.star.michelet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicheletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicheletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/star/michelet/MicheletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.michelet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicheletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
