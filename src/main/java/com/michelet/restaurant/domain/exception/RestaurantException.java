5f203c15e055c291d5caa9c559e8caefc750482d
jihoon <git4ub0@gmail.com>
2026-04-28 01:34:15 +0900

    feat: Restaurant 도메인 예외 추가

--- src/main/java/com/michelet/restaurant/domain/exception/RestaurantException.java
+++ src/main/java/com/michelet/restaurant/domain/exception/RestaurantException.java
@@ -1,7 +1,6 @@
 package com.michelet.restaurant.domain.exception;
 
 import com.michelet.common.exception.BusinessException;
-import com.michelet.common.exception.ErrorCode;
 
 public class RestaurantException extends BusinessException {
 
