10a2777dcbb89904f1906a4256eefac096624ee0
jihoon <git4ub0@gmail.com>
2026-04-27 19:48:53 +0900

    feat: JPA Auditing 설정 추가

---
+++ src/main/java/com/michelet/restaurant/infrastructure/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.michelet.restaurant.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class JpaAuditingConfig {
+}
