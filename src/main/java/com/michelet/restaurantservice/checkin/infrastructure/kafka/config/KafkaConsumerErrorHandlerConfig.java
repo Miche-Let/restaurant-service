2f0dde0e6dea63a05d10b8fdc7186f781a086bde
jihoon <git4ub0@gmail.com>
2026-05-10 12:25:26 +0900

    refactor: Configuration 어노테이션 추가

--- src/main/java/com/michelet/restaurantservice/checkin/infrastructure/kafka/config/KafkaConsumerErrorHandlerConfig.java
+++ src/main/java/com/michelet/restaurantservice/checkin/infrastructure/kafka/config/KafkaConsumerErrorHandlerConfig.java
@@ -4,10 +4,12 @@ package com.michelet.restaurantservice.checkin.infrastructure.kafka.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.listener.CommonErrorHandler;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
+@Configuration
 public class KafkaConsumerErrorHandlerConfig {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumerErrorHandlerConfig.class);
